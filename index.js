6093ac0105d71ff33d9e6c219bb4d38a70891d2d
yego <yegokipkemoi254@gmail.com>
2022-08-10 12:24:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let message = [];
+    let n = names.length;
+    for (let i = 0; i < n; i++) {
+        message[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return message;
+  }
+  function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+  }
